67a4c6de79fabb6c37b8c007a3d9f49874d15992
theeraneng <theerapongsurapol@gmail.com>
2018-06-16 18:19:53 +0700

    welcome

--- mgbook/resources/views/welcome.blade.php
+++ mgbook/resources/views/welcome.blade.php
@@ -1,19 +1,3 @@
-<!doctype html>
 
-
-            <div class="content">
-                <div class="title m-b-md">
                     Theerapong
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">test</a>
-                    <a href="https://laracasts.com">test</a>
-                    <a href="https://laravel-news.com">test</a>
-                    <a href="https://forge.laravel.com">test</a>
-                    <a href="https://github.com/laravel/laravel">test</a>
-                </div>
-            </div>
-        </div>
-    </body>
-</html>
+  
